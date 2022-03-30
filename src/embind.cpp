0858131f100e3e5aa7b80bcef0db604c9f8111b1
Mark Gillespie <mark.gillespie81@gmail.com>
2022-03-30 17:31:04 -0400

    Compile to wasm

---
+++ src/embind.cpp
@@ -0,0 +1,96 @@
+#include "geometrycentral/numerical/linear_solvers.h"
+#include "geometrycentral/surface/manifold_surface_mesh.h"
+#include "geometrycentral/surface/meshio.h"
+#include "geometrycentral/surface/simple_polygon_mesh.h"
+#include "geometrycentral/surface/vertex_position_geometry.h"
+
+#include <emscripten/bind.h>
+#include <emscripten/val.h>
+
+#include <Eigen/Dense>
+#include <Eigen/SparseCore>
+
+#include "geometrycentral/surface/manifold_surface_mesh.h"
+#include "geometrycentral/surface/simple_polygon_mesh.h"
+
+#include "interpolate_vectors.h"
+
+using namespace emscripten;
+using namespace geometrycentral;
+using namespace geometrycentral::surface;
+
+struct GeoMesh {
+    std::unique_ptr<ManifoldSurfaceMesh> mesh;
+    std::unique_ptr<VertexPositionGeometry> geom;
+};
+
+std::vector<Vector3> toList(ManifoldSurfaceMesh& mesh,
+                            const VertexData<Vector3>& data) {
+    std::vector<Vector3> vectorList;
+    for (Vertex v : mesh.vertices()) {
+        vectorList.push_back(data[v]);
+    }
+    return vectorList;
+}
+
+std::vector<Vector3> generateSmoothBoundaryField(GeoMesh& geo) {
+    VertexData<Vector3> bField =
+        generateSmoothBoundaryVectorField(*geo.mesh, *geo.geom);
+    return toList(*geo.mesh, bField);
+}
+
+std::vector<Vector3> generateWavyBoundaryField(GeoMesh& geo, size_t frequency) {
+    VertexData<Vector3> bField =
+        generateWavyBoundaryVectorField(*geo.mesh, *geo.geom, frequency);
+    return toList(*geo.mesh, bField);
+}
+
+// Stolen from Ricky Reusser https://observablehq.com/d/d0df0c04ce5c94FCC
+template <typename T>
+void copyToVector(const val& typedArray, std::vector<T>& vec) {
+    unsigned int length = typedArray["length"].as<unsigned int>();
+    val memory          = val::module_property("buffer");
+    vec.reserve(length);
+    val memoryView = typedArray["constructor"].new_(
+        memory, reinterpret_cast<uintptr_t>(vec.data()), length);
+    memoryView.call<void>("set", typedArray);
+}
+
+// Mostly stolen from Ricky Reusser https://observablehq.com/d/d0df0c04ce5c94fc
+EMSCRIPTEN_BINDINGS(my_module) {
+    value_array<Vector3>("Vector3")
+        .element(&Vector3::x)
+        .element(&Vector3::y)
+        .element(&Vector3::z);
+    value_array<Vector2>("Vector2").element(&Vector2::x).element(&Vector2::y);
+
+    register_vector<Vector3>("VectorVector3");
+    register_vector<size_t>("VectorSizeT");
+    register_vector<std::vector<size_t>>("VectorVectorSizeT");
+
+    class_<GeoMesh>("GCMesh")
+        .function("polygons", optional_override([](GeoMesh& self) {
+                      return self.mesh->getFaceVertexList();
+                  }))
+        .function("vertexCoordinates",
+                  optional_override([](const GeoMesh& self) {
+                      std::vector<Vector3> vCoords;
+                      for (Vertex v : self.mesh->vertices())
+                          vCoords.push_back(self.geom->inputVertexPositions[v]);
+                      return vCoords;
+                  }));
+
+    function("readMesh",
+             optional_override([](std::string str, std::string type = "") {
+                 std::stringstream in;
+                 in << str;
+
+                 GeoMesh gMesh;
+                 std::tie(gMesh.mesh, gMesh.geom) =
+                     readManifoldSurfaceMesh(in, type);
+                 return gMesh;
+             }));
+
+    function("generateSmoothBoundaryField", &generateSmoothBoundaryField);
+    function("generateWavyBoundaryField", &generateWavyBoundaryField);
+}
