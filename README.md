7f1f61fae5688f56dfb0dfa369e58db3c0cfd5f7
Mark Gillespie <mark.gillespie81@gmail.com>
2022-03-31 13:33:10 -0400

    Add some example images

--- README.md
+++ README.md
@@ -1,18 +1,54 @@
-# GeometryTemplate
-A template project to get started with geometry-central and Polyscope. Copied from Nick's template [here](https://github.com/nmwsharp/gc-polyscope-project-template), but with code for googletest added in.
+# Vector Interpolator
 
-This repo is set up as a template, but it seems the submodules don't get copied to the new project. Run `setup.sh` to set up the required submodules and to run cmake. Setup also sets up cmake to export compile commands for `clang-format`. To build the code, you can run
-```
+Try the online demo [here](https://markjgillespie.com/Demos/VectorInterpolation/html/)!
+
+![A vector field interpolated across a 3d model of a hand.](images/ui-example.png)
+
+This repo provides a comparison of several different ways of interpolating 3D unit vector fields across a surface mesh given some data on the boundary. In general, all of the methods except for *connection Laplacian interpolation* seem to behave quite similarly, but using the connection Laplacian can yield very different results.
+
+### Harmonic interpolation
+![harmonic interpolation across hand](images/interpolation-harmonic.png)
+Interpolates the 3 coordinates of the vector field independently via harmonic functions and normalizes the resulting vector field.
+
+### Connection Laplacian interpolation
+![connection Laplacian interpolation across hand](images/interpolation-connection.png)
+![connection Laplacian + scalar Laplaician interpolation across hand](images/interpolation-connection++.png)
+Splits the boundary data into a tangential vector field plus a normal component. The tangential field is interpolated by a harmonic tangent vector field (using the [connection Laplacian](http://geometry-central.net/surface/geometry/quantities/#vertex-connection-laplacian)). The normal component can either be chosen to make the resulting field a unit vector field, or can be interpolated separately by a separate scalar function. The latter option involves slightly more computation, but has the advantage of allowing the normal field to point into the surface and out of the surface at different points. 
+
+### Stereographic projection
+![stereographic interpolation across hand](images/interpolation-stereographic.png)
+Uses stereographic projection to express the given unit vectors on the boundary as 2D vectors in the plane. Then interpolates those 2D vectors harmonically across the interior of the mesh, and uses inverse stereographic projection to obtain a unit vector field on the interior of the mesh.
+
+### Harmonic map to the sphere
+![stereographic interpolation across hand](images/interpolation-harmonic-sphere.png)
+Computes a harmonic map to the unit sphere.
+
+
+## Building
+To compile the code, use
+``` bash
+mkdir build
 cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+```
+
+### Javascript Version
+This can also be compiled to Javascript via [Emscripten](https://emscripten.org/docs/getting_started/downloads.html).
+
+Once you have Emscripten installed, then building this code is just like building an ordinary `geometry-central` project. Run the following commands from the `build` directory.
+``` bash
+emcmake cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=emcc -DCMAKE_CXX_COMPILER=em++ -DEMSCRIPTEN=True ..
+emmake make -j7
+```
+This creates two "binaries", `bin/embind.js` and `bin/embind.wasm`. Then, you should copy these files into the `html` directory.
+
+Because of some browser stuff, these `embind` files need to be served from a server - the page doesn't work if you just open the file locally.
+
+Note that I had to include Eigen as an explicit dependency. Emscripten didn't like geometry-central's fancy on-the-fly Eigen downloader. But if you just include Eigen as a dependency everything works fine.
+
+### Switching back from Javascript to local binaries
+If you want to compile the ordinary command line version after building the javascript version, you have to tell CMake to use your C compiler again
+``` bash
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DEMSCRIPTEN=False ..
 make -j7
-```
-
-Then run the code with
-```
-bin/run /path/to/a/mesh
-```
-
-Run the tests with
-```
-bin/test
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -29,43 +29,68 @@ void myCallback() {
     if (ImGui::Button("Interpolate by harmonic function")) {
         VertexData<Vector3> interpolatedNormals =
             interpolateByHarmonicFunction(*mesh, *geom, boundaryData);
-        psMesh->addVertexVectorQuantity("harmonic interpolation",
-                                        interpolatedNormals);
+        psMesh
+            ->addVertexVectorQuantity("harmonic interpolation",
+                                      interpolatedNormals)
+            ->setVectorLengthScale(0.05)
+            ->setVectorRadius(0.0075);
     }
     if (ImGui::Button("Interpolate by connection laplacian")) {
         VertexData<Vector3> interpolatedNormals =
             interpolateByConnectionLaplacian(*mesh, *geom, boundaryData, false);
-        psMesh->addVertexVectorQuantity(
-            "pure connection laplacian interpolation", interpolatedNormals);
+        psMesh
+            ->addVertexVectorQuantity("pure connection laplacian interpolation",
+                                      interpolatedNormals)
+            ->setVectorLengthScale(0.05)
+            ->setVectorRadius(0.0075);
+        ;
     }
     if (ImGui::Button(
             "Interpolate by connection laplacian + scalar laplacian")) {
         VertexData<Vector3> interpolatedNormals =
             interpolateByConnectionLaplacian(*mesh, *geom, boundaryData, true);
-        psMesh->addVertexVectorQuantity(
-            "connection laplacian + scalar laplacian interpolation",
-            interpolatedNormals);
+        psMesh
+            ->addVertexVectorQuantity(
+                "connection laplacian + scalar laplacian interpolation",
+                interpolatedNormals)
+            ->setVectorLengthScale(0.05)
+            ->setVectorRadius(0.0075);
+        ;
     }
     if (ImGui::Button("Interpolate by stereographic projection")) {
         VertexData<Vector3> interpolatedNormals =
             interpolateByStereographicProjection(*mesh, *geom, boundaryData);
-        psMesh->addVertexVectorQuantity("stereographic interpolation",
-                                        interpolatedNormals);
+        psMesh
+            ->addVertexVectorQuantity("stereographic interpolation",
+                                      interpolatedNormals)
+            ->setVectorLengthScale(0.05)
+            ->setVectorRadius(0.0075);
+        ;
     }
     if (ImGui::Button("Interpolate by harmonic map to sphere")) {
         VertexData<Vector3> interpolatedNormals =
             interpolateByHarmonicMapToSphere(*mesh, *geom, boundaryData);
-        psMesh->addVertexVectorQuantity("harmonic sphere map",
-                                        interpolatedNormals);
+        psMesh
+            ->addVertexVectorQuantity("harmonic sphere map",
+                                      interpolatedNormals)
+            ->setVectorLengthScale(0.05)
+            ->setVectorRadius(0.0075);
+        ;
     }
     ImGui::Separator();
     if (ImGui::Button("Set smooth boundary conditions")) {
         boundaryData = generateSmoothBoundaryVectorField(*mesh, *geom);
-        psMesh->addVertexVectorQuantity("boundary data", boundaryData);
+        psMesh->addVertexVectorQuantity("boundary data", boundaryData)
+            ->setVectorLengthScale(0.05)
+            ->setVectorRadius(0.0075);
+        ;
     }
     if (ImGui::Button("Set wavy boundary conditions")) {
         boundaryData = generateWavyBoundaryVectorField(*mesh, *geom);
-        psMesh->addVertexVectorQuantity("boundary data", boundaryData);
+        psMesh->addVertexVectorQuantity("boundary data", boundaryData)
+            ->setVectorLengthScale(0.05)
+            ->setVectorRadius(0.0075);
+        ;
     }
 }
 
@@ -114,11 +139,15 @@ int main(int argc, char** argv) {
     }
 
     // Register the mesh with polyscope
-    psMesh       = polyscope::registerSurfaceMesh("mesh", geom->vertexPositions,
+    psMesh = polyscope::registerSurfaceMesh("mesh", geom->vertexPositions,
                                             mesh->getFaceVertexList(),
                                             polyscopePermutations(*mesh));
+    psMesh->setSmoothShade(true);
     boundaryData = generateSmoothBoundaryVectorField(*mesh, *geom);
-    psMesh->addVertexVectorQuantity("boundary data", boundaryData);
+    psMesh->addVertexVectorQuantity("boundary data", boundaryData)
+        ->setVectorLengthScale(0.05)
+        ->setVectorRadius(0.0075);
+    ;
 
     // Give control to the polyscope gui
     polyscope::show();
